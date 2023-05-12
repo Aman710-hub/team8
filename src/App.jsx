5aebdcbeabebb263c70099ba8c5528e887fa66c1
Amantur <amantur710@gmail.com>
2023-05-13 00:22:40 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
+
+function App() {
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+}
+
+export default App;
